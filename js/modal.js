a90202e20649c1c3a94bba17d3e5ebd229d6dc35
William Vitorino <williamvitorino3@gmail.com>
2017-09-29 13:46:32 -0300

    Vue componente removido.

--- js/modal.js
+++ js/modal.js
@@ -2,7 +2,7 @@
 let modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-let btns = document.getElementsByClassName("btnAdd");
+let btns = document.getElementsByClassName("btn");
 
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
