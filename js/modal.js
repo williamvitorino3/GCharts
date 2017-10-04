007526152d576a5e681cbcb9e04c7776278770ee
William Vitorino <williamvitorino3@gmail.com>
2017-10-03 21:16:15 -0300

    Função de esconder o modal concertada.

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,12 @@
 // Get the modal
 let modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-let btns = document.getElementsByClassName("openModal");
-
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("clsModal");
 
 // When the user clicks on the button, open the modal 
-for (let i = 0; i < btns.length; i++) {
-    btns[i].onclick = function () {
+for (btn of document.getElementsByClassName("openModal")) {
+    btn.onclick = function () {
         modal.style.display = "block";
     }
 }
@@ -20,7 +17,7 @@ function closeModal(event) {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal || event.target == span) {
+    if (event.target == modal) {
         closeModal();
     }
 }
